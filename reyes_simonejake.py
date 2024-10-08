a6e72cb54ebd82d1779cda92f8949bd6fde26c2f
mamasalanang-gerald <geraldkasan163@gmail.com>
2024-10-08 19:03:20 +0800

    Send Message to Daniel

--- reyes_simonejake.py
+++ reyes_simonejake.py
@@ -4,5 +4,6 @@ print("Gender: Male")
 print("Email: reyesjake262@gmail.com")
 
 print("fin fin fin fin")
+print("Hi MONDI MAMONMON!")
 print("ow ow woow")
 print("This is a dedication message from - mosquito")
