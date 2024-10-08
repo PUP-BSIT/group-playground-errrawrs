4b301d86b511c048b7538d76a37a002132169596
reyes-simonejake <125109979+reyes-simonejake@users.noreply.github.com>
2024-10-08 18:02:59 +0800

    update - simone

--- gerald.py
+++ gerald.py
@@ -2,3 +2,5 @@ print("Name: Gerald Mamasalanang")
 print("Birthday: October 26, 2005") 
 print("Gender: Male")
 print("Email: geraldkasan163@gmail")
+
+print("This is a dedication message from simone pogi")
