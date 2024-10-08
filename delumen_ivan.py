86ac6d1e81f4499f930031d0c64ee2379debfaa5
mosquito-michaelangelo <michaelmosquito147@gmail.com>
2024-10-08 18:42:46 +0800

    upload this python file

--- delumen_ivan.py
+++ delumen_ivan.py
@@ -2,3 +2,5 @@ print("Name: Ivan Virrey Delumen")
 print("Birthday: October 28, 2004")
 print("Gender: Male")
 print("Email: ivandelumen05@gmail.com")
+
+print("This is a dedication message from mosquito")
