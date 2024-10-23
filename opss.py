53a5d51fc6f064b519f9b2e045a0a760e18f9259
mosquito-michaelangelo <michaelmosquito147@gmail.com>
2024-10-23 15:45:28 +0800

    update again in opss

--- opss.py
+++ opss.py
@@ -3,9 +3,9 @@ all_items = []
 # Product details
 def enter_product_details():
     global product_name, product_price, product_quantity
-    product_name = input("a. Product Name: ")
-    product_price = int(input("b. Price: "))
-    product_quantity = int(input("c. Quantity: "))
+    product_name = input("Product Name: ")
+    product_price = int(input("Price: "))
+    product_quantity = int(input("Quantity: "))
     total = product_price * product_quantity
 
     return [product_name, product_price, product_quantity, total]
