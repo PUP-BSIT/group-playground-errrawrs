848fb7bfb3fdc86f1a172ea29683364506267682
victorioso-daniel <danielvictorioso.acd@gmail.com>
2024-10-23 17:18:59 +0800

    update file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#function for product details
+#function for product details - Simone 
 def order_take():
     global product_name, product_price, product_quantity
     product_name = input("Product Name: ")
@@ -8,7 +8,7 @@ def order_take():
 
     return [product_name, product_price, product_quantity, total]
 
-#function for senior citizen details
+#function for senior citizen details - Daniel and Gerald
 def senior_citizen_details(total_amount,senior_id_no):
 
     total_amount = sum(item[3] for item in all_items)  
@@ -23,7 +23,7 @@ all_items = [] #global list
 
 is_order_again = True
 
-#asking for another order
+#asking for another order - Ivan
 while is_order_again:
     all_items.append(order_take())
     user_prompt = input("Would you like to add another order? y - YES / n - NO: ")
@@ -31,13 +31,13 @@ while is_order_again:
 
 total_amount = sum(item[3] for item in all_items)  
 
-#buyer details and senior citizen details
+#buyer details and senior citizen details - Daniel and Gerald
 buyer_name = input("\nWhat is your name: ")
 senior_id_no = input("Senior ID no. (leave blank if not a senior): ")
 
 total_amount = senior_citizen_details(total_amount, senior_id_no)
 
-#Total list and amount of order
+#Total list and amount of order - Michael
 for item in all_items:
     print(f"\nItems: {item[0]}, Price: {item[1]}, Quantity: {item[2]}, Total: {item[3]}")
 
