0ee49a4d80b761f354c18e4b015215e2012f6dd9
mosquito-michaelangelo <michaelmosquito147@gmail.com>
2024-11-26 17:46:10 +0800

    fix mosquito module

--- mypackage/mosquito.py
+++ mypackage/mosquito.py
@@ -6,8 +6,21 @@ def date_function():
     next_day = D.tomorrow()
     last_day = D.yesterday()
 
-    print("This is the date today:",date_now)   
+    print("Enter 1 if date today")
+    print("Enter 2 if date tomorrow")
+    print("Enter 3 if date yesterday")
+    print("Enter 4 if exit")
 
-    print("This is the date tomorrow:",next_day)
-
-    print("This is the date yesterday:", last_day)
+    while True:
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            print("This is the date today:",date_now)   
+        elif choice == "2":
+            print("This is the date tomorrow:",next_day)
+        elif choice == "3":
+            print("This is the date yesterday:", last_day)
+        elif choice == "4":
+            print("Exiting this module")
+            break
+        else:
+            print("Choice is invalid!!!")
