2edaf4b769b7db2495672228cee03a07a11bf20c
mosquito-michaelangelo <michaelmosquito147@gmail.com>
2024-11-27 10:33:33 +0800

    fix module

--- mypackage/mosquito.py
+++ mypackage/mosquito.py
@@ -1,28 +1,36 @@
-from datetime import date
 from beautiful_date import D
 
-def date_function():
-    date_now = date.today()
-    next_day = D.tomorrow()
-    last_day = D.yesterday()
-
+def display_dates():
     print("Enter 1 if date today")
     print("Enter 2 if date tomorrow")
     print("Enter 3 if date yesterday")
     print("Enter 4 if exit")
 
-    while True:
+def date_function():
+    date_now = D.today()
+    next_day = D.tomorrow()
+    last_day = D.yesterday()
 
+    while True:
+        display_dates()
+        
         choice = input("Enter your choice: ")
+
+        dmy_format_today = f"{date_now.day}/{date_now.month}/{date_now.year}"
+        dmy_format_tomorrow = f"{next_day.day}/{next_day.month}/{next_day.year}"
+        dmy_format_yesterday = f"{last_day.day}/{last_day.month}/{last_day.year}"
         
         if choice == "1":
-            print("This is the date today:",date_now)   
+            print("\n")
+            print(f"This is the date today: {dmy_format_today}\n")   
         elif choice == "2":
-            print("This is the date tomorrow:",next_day)
+            print("\n")
+            print(f"This is the date tomorrow: {dmy_format_tomorrow}\n")
         elif choice == "3":
-            print("This is the date yesterday:", last_day)
+            print("\n")
+            print(f"This is the date yesterday: {dmy_format_yesterday}\n")
         elif choice == "4":
-            print("Exiting this module")
+            print("Exiting....")
             break
         else:
             print("Choice is invalid!!!")
